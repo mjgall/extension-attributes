4cad4ae1222262d317f56f2d69d4dde11a76fc78
Mike Gallagher <mike.gallagher@MacBook-Pro-2.local>
2020-10-30 12:36:06 -0500

    ISO output

--- lastReboot.sh
+++ lastReboot.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-# From: https://www.jamf.com/jamf-nation/discussions/9623/create-an-extension-attribute-that-reports-last-reboot-for-a-mac
+# https://gist.github.com/talkingmoose/f89b71bc85537fef8882038c8714b160
 
-lastboot=`last reboot | tail -3 | head -1 | awk '{ print $6 " " $3 ", " $4 " " $5 }'`
+# get last reboot date and time
+lastReboot=$( who -b )
 
-echo "<result>$lastboot</result>"
+# extract month, day and time
+month=$( /usr/bin/awk '{ print $3 }' <<< "$lastReboot" )
+day=$( /usr/bin/awk '{ print $4 }' <<< "$lastReboot" )
+time=$( /usr/bin/awk '{ print $5 }' <<< "$lastReboot" )
+
+# convert date and time to ISO date format
+lastRebootISO=$( /bin/date -j -f "%b %e %l:%M" "$month $day $time" '+%Y-%m-%d %I:%M:00' )
+
+echo "<result>$lastRebootISO</result>"
+
+exit 0
