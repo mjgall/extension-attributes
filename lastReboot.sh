e5b253f9da95658aea6602ce9e41c8df29056bfc
Mike Gallagher <mike.gallagher@MacBook-Pro-2.local>
2020-10-30 11:27:28 -0500

    credit to jn post

--- lastReboot.sh
+++ lastReboot.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# From: https://www.jamf.com/jamf-nation/discussions/9623/create-an-extension-attribute-that-reports-last-reboot-for-a-mac
+
 lastboot=`last reboot | tail -3 | head -1 | awk '{ print $6 " " $3 ", " $4 " " $5 }'`
 
 echo "<result>$lastboot</result>"
